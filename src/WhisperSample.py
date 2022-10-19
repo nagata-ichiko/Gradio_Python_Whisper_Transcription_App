9ce44fd533d1399adc99bd1feaf34d6d1930c6ca
nagata-ichiko <nagataichiko2249@gmail.com>
2022-10-19 23:59:06 +0900

    Whisperサンプルアプリケーション ・30秒単位の文字起こしに対応 ・音声ファイルに対応 ・動画ファイルに対応 ・マイク入力に対応

---
+++ src/WhisperSample.py
@@ -0,0 +1,37 @@
+from multiprocessing.sharedctypes import Value
+import whisper
+import gradio as gr 
+
+model = whisper.load_model("base")
+
+def speechRecognitionModel(input): 
+    # 30秒データに変換
+    audio = whisper.load_audio(input)
+    audio = whisper.pad_or_trim(audio)
+
+    mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+    _, probs = model.detect_language(mel)
+    print(f"言語: {max(probs, key=probs.get)}")
+
+    # 文字起こし
+    options = whisper.DecodingOptions(fp16=False)
+    result = whisper.decode(model, mel, options)
+
+    return result.text
+
+gr.Interface(
+    title = 'Whisper Sample App', 
+    fn=speechRecognitionModel, 
+    inputs=[
+        # 音声ファイル
+        gr.Audio(type="filepath")
+        # 動画ファイル
+        # gr.Video(type="filepath")
+        # マイク入力
+        # gr.inputs.Audio(source="microphone", type="filepath")        
+    ],
+    outputs=[
+        "textbox"
+    ],
+    live=True).launch()
