87c23f98ceac7d0e3621850a9d72c287dfa31dcb
nagata-ichiko <nagataichiko2249@gmail.com>
2022-10-20 23:26:14 +0900

    30秒以上の文字起こしに対応。コンソールログ出力のみ

--- src/WhisperSample.py
+++ src/WhisperSample.py
@@ -6,19 +6,22 @@ model = whisper.load_model("base")
 
 def speechRecognitionModel(input): 
     # 30秒データに変換
-    audio = whisper.load_audio(input)
-    audio = whisper.pad_or_trim(audio)
+    # audio = whisper.load_audio(input)
+    # audio = whisper.pad_or_trim(audio)
 
-    mel = whisper.log_mel_spectrogram(audio).to(model.device)
+    # mel = whisper.log_mel_spectrogram(audio).to(model.device)
 
-    _, probs = model.detect_language(mel)
-    print(f"言語: {max(probs, key=probs.get)}")
+    # _, probs = model.detect_language(mel)
+    # print(f"言語: {max(probs, key=probs.get)}")
 
-    # 文字起こし
-    options = whisper.DecodingOptions(fp16=False)
-    result = whisper.decode(model, mel, options)
-
-    return result.text
+    # # 文字起こし
+    # options = whisper.DecodingOptions(fp16=False)
+    # result = whisper.decode(model, mel, options)
+    
+    #　時間制限なし、しかし画面表示が行われない
+    result = model.transcribe(input, verbose=True, language="ja")
+    
+    return result
 
 gr.Interface(
     title = 'Whisper Sample App', 
