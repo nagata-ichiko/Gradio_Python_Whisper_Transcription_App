bc60d1247cbd2875abaaee2ec669e6ba40c5f83a
Nagata Ichiko <106644839+nagata-ichiko@users.noreply.github.com>
2023-03-21 00:44:15 +0900

    ブランチを初期状態に

--- src/TranscriptionGradioWhisper.py
+++ src/TranscriptionGradioWhisper.py
@@ -1,6 +1,4 @@
 from multiprocessing.sharedctypes import Value
-from faster_whisper import WhisperModel
-model_path = "whisper-large-v2-ct2/"
 import whisper
 import gradio as gr 
 from datetime import timedelta
@@ -10,13 +8,9 @@ import srt
 # モデル選択、下に行くほどデカくて遅いが高精度
 # model = whisper.load_model("tiny")
 # model = whisper.load_model("base")
-# model = whisper.load_model("small")
+model = whisper.load_model("small")
 # model = whisper.load_model("medium")
 # model = whisper.load_model("large")
-
-# 高速モデル https://github.com/guillaumekln/faster-whisper
-# ct2-transformers-converter --model openai/whisper-large-v2 --output_dir whisper-large-v2-ct2 実行
-model = WhisperModel(model_path, device="cpu", compute_type="int8")
 
 def speechRecognitionModel(input): 
     # 30秒データに変換
@@ -32,24 +26,17 @@ def speechRecognitionModel(input):
     # options = whisper.DecodingOptions(fp16=False)
     # result = whisper.decode(model, mel, options)
     
+    #　時間制限なし、しかし画面表示が行われない
+    result = model.transcribe(input, verbose=True, language="ja")
     
-    segments, _ = model.transcribe(input, word_timestamps=True)
-
-    # for segment in segments:
-    #     for word in segment.words:
-    #         print("[%.2fs -> %.2fs] %s" % (word.start, word.end, word.word))
-    
-    result = model.transcribe(input, language="ja")
-    
-    # seginfo = result["segments"]
-    seginfo = result
+    seginfo = result["segments"]
     out_text = []
 
     # segment情報から発言の開始/終了時間とテキストを抜き出し、srt形式で編集する
-    for segment in seginfo:
-        start = segment.start
-        end = segment.end
-        text = segment.text
+    for data in seginfo:
+        start = data["start"]
+        end = data["end"]
+        text = data["text"]
         out_line = Subtitle(index=1,\
                             start=timedelta(seconds=timedelta(seconds=start).seconds,\
                             microseconds=timedelta(seconds=start).microseconds),\
@@ -67,7 +54,6 @@ def speechRecognitionModel(input):
         f.write(origin)
     
     return result
-    # return ""
 
 gr.Interface(
     title = 'Whisper Sample App', 
